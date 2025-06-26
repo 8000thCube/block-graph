10ac2bd3f055b34d8e92f4a69e91b9179a392579
Aili Nierengarten <125620224+8000thCube@users.noreply.github.com>
2025-06-25 20:44:03 -0700

    Update graph.rs

--- src/graph.rs
+++ src/graph.rs
@@ -1,26 +1,73 @@
-impl<C:AI<V,V>,V:Default+Merge> AI<Vec<V>,Vec<V>> for Graph<C>{
-	fn forward(&self,input:Vec<V>)->Vec<V>{//TODO fix hio somehow
-		let (connections,connectivity)=(&self.connections,&self.connectivity);
-		let (hidden,inputs,outputs)=(self.hidden,self.inputs,self.outputs);
-		let mut slots=input;
-		slots.resize_with(hidden+inputs+outputs,Default::default);
+impl Default for GraphIO{
+	fn default()->Self{Self::Hidden(0)}
+}
+impl From<GraphIn> for GraphIO{
+	fn from(GraphIn(v):GraphIn)->Self{Self::Input(v)}
+}
+impl From<GraphOut> for GraphIO{
+	fn from(GraphOut(v):GraphOut)->Self{Self::Output(v)}
+}
+impl From<usize> for GraphIO{
+	fn from(v:usize)->Self{Self::Hidden(v)}
+}
+impl GraphIO{
+	#[track_caller]
+	/// encodes as an isize with !n for inputs or outputs and n for hidden
+	fn as_input_isize(self)->isize{
+		(match self{Self::Hidden(n)=>n,Self::Input(n)=>!n,Self::Output(_n)=>panic!("cannot read from graph output. try specifying an input instead")}) as isize
+	}
+	#[track_caller]
+	/// encodes as an isize with !n for inputs or outputs and n for hidden
+	fn as_output_isize(self)->isize{
+		(match self{Self::Hidden(n)=>n,Self::Input(_n)=>panic!("cannot write to graph input. try specifying an output instead"),Self::Output(n)=>!n}) as isize
+	}
+}
+impl VertexLabels{
+	fn _produce_index<S:Into<Cow<'static,str>>>(&mut self,label:S)->usize{
+		let Self(map)=self;
+		let nextid=map.len();
 
+		*map.entry(label.into()).or_insert(nextid)
+	}
+	/// gets or adds an index associated with the str.
+	pub fn labeled(&mut self,label:&'static str)->usize{self._produce_index(label)}
+	/// creates a new listing of vertex labels
+	pub fn new()->Self{Self(HashMap::new())}
+	/// gets or adds an index associated with the string
+	pub fn produce_index(&mut self,label:String)->usize{self._produce_index(label)}
+}
+impl<C:AI<V,V>,V:Default+Merge> AI<Vec<V>,Vec<V>> for Graph<C>{
+	fn forward(&self,input:Vec<V>)->Vec<V>{
+		let (connections,connectivity)=(&self.connections,&self.connectivity);
+		let hidden=connectivity.iter().flat_map(|&(_connection,input,output)|[input,output]).filter(|&x|x>=0).max().map(|x|x as usize+1).unwrap_or(0);
+		let inputs=connectivity.iter().map(|&(_connection,input,_output)|!input).filter(|&x|x>=0).max().map(|x|x as usize+1).unwrap_or(0);
+		let outputs=connectivity.iter().map(|&(_connection,_input,output)|!output).filter(|&x|x>=0).max().map(|x|x as usize+1).unwrap_or(0);
+		let mut slots=input;
+
+		slots.resize_with(hidden+inputs+outputs,Default::default);
+		let (i,o)=slots.split_at_mut(inputs);
+		let (h,o)=o.split_at_mut(hidden);
 		connectivity.iter().for_each(|&(c,x,y)|{
-			let x=take(&mut slots[x]);
-			slots[y].merge(connections[c].forward(x));
+			let x=take(if x<0{&mut i[!x as usize]}else{&mut h[x as usize]});
+			if y<0{&mut o[!y as usize]}else{&mut h[y as usize]}.merge(connections[c].forward(x));
 		});
 		slots.drain(0..hidden+inputs);
 		slots
 	}
 	fn forward_mut(&mut self,input:Vec<V>)->Vec<V>{
-		let (connections,connectivity)=(&mut self.connections,&self.connectivity);
-		let (hidden,inputs,outputs)=(self.hidden,self.inputs,self.outputs);
+		let connections=&mut self.connections;
+		let connectivity=&self.connectivity;
+		let hidden=connectivity.iter().flat_map(|&(_connection,input,output)|[input,output]).filter(|&x|x>=0).max().map(|x|x as usize+1).unwrap_or(0);
+		let inputs=connectivity.iter().map(|&(_connection,input,_output)|!input).filter(|&x|x>=0).max().map(|x|x as usize+1).unwrap_or(0);
+		let outputs=connectivity.iter().map(|&(_connection,_input,output)|!output).filter(|&x|x>=0).max().map(|x|x as usize+1).unwrap_or(0);
 		let mut slots=input;
-		slots.resize_with(hidden+inputs+outputs,Default::default);
 
+		slots.resize_with(hidden+inputs+outputs,Default::default);
+		let (i,o)=slots.split_at_mut(inputs);
+		let (h,o)=o.split_at_mut(hidden);
 		connectivity.iter().for_each(|&(c,x,y)|{
-			let x=take(&mut slots[x]);
-			slots[y].merge(connections[c].forward_mut(x));
+			let x=take(if x<0{&mut i[!x as usize]}else{&mut h[x as usize]});
+			if y<0{&mut o[!y as usize]}else{&mut h[y as usize]}.merge(connections[c].forward_mut(x));
 		});
 		slots.drain(0..hidden+inputs);
 		slots
@@ -29,12 +76,11 @@ impl<C:AI<V,V>,V:Default+Merge> AI<Vec<V>,Vec<V>> for Graph<C>{
 impl<C:Decompose> Decompose for Graph<C>{
 	fn compose((connections,connectivity):Self::Decomposition)->Self{
 		let connections:Vec<C>=connections.into_iter().map(C::compose).collect();
-		let mut connectivity:Vec<(usize,usize,usize)>=connectivity.chunks_exact(3).map(|x|(x[0],x[1],x[2])).collect();
-		let (hidden,inputs,outputs)=connectivity.pop().unwrap();
-		Self{connections,connectivity,hidden,inputs,outputs}
+		let connectivity:Vec<(usize,isize,isize)>=connectivity.chunks_exact(3).map(|x|(x[0],x[1] as isize,x[2] as isize)).collect();
+		Self{connections,connectivity}
 	}
-	fn decompose(self)->Self::Decomposition{(self.connections.into_iter().map(C::decompose).collect(),self.connectivity.into_iter().chain([(self.hidden,self.inputs,self.outputs)]).flat_map(|(x,y,z)|[x,y,z]).collect())}
-	fn decompose_cloned(&self)->Self::Decomposition{(self.connections.iter().map(C::decompose_cloned).collect(),self.connectivity.iter().copied().chain([(self.hidden,self.inputs,self.outputs)]).flat_map(|(x,y,z)|[x,y,z]).collect())}
+	fn decompose(self)->Self::Decomposition{(self.connections.into_iter().map(C::decompose).collect(),self.connectivity.into_iter().flat_map(|(x,y,z)|[x,y as usize,z as usize]).collect())}
+	fn decompose_cloned(&self)->Self::Decomposition{(self.connections.iter().map(C::decompose_cloned).collect(),self.connectivity.iter().copied().flat_map(|(x,y,z)|[x,y as usize,z as usize]).collect())}
 	type Decomposition=(Vec<C::Decomposition>,Vec<usize>);
 }
 impl<C:Op> Op for Graph<C>{
@@ -42,44 +88,70 @@ impl<C:Op> Op for Graph<C>{
 }
 impl<C> Default for Graph<C>{
 	fn default()->Self{
-		Self{connections:Vec::new(),connectivity:Vec::new(),hidden:0,inputs:0,outputs:0}
+		Self{connections:Vec::new(),connectivity:Vec::new()}
 	}
 }
 impl<C> Graph<C>{
 	/// creates a new empty graph
 	pub const fn new()->Self{
-		Self{connections:Vec::new(),connectivity:Vec::new(),hidden:0,inputs:0,outputs:0}
+		Self{connections:Vec::new(),connectivity:Vec::new()}
 	}
-	/// adds a connection between two vertices
-	pub fn add_connection(&mut self,connection:C,input:usize,output:usize)->usize{
-		(self.hidden,self.inputs,self.outputs)=(0,0,0);
-		let cx=self.connectivity.len();
-		self.connections.push(connection);
-		self.connectivity.push((cx,input,output));
-		cx
+	/// adds a connection that connects two vertices. The connections run in the order they are connected.
+	pub fn add_connection<A:Into<C>,I:Into<GraphIO>,J:Into<GraphIO>>(&mut self,connection:A,input:I,output:J)->usize{
+		let connection=self.push(connection);
+		self.connect(connection,input,output);
+		connection
 	}
-	/// connects two graph between vertices with the connection specified by index. The connections run in the order they are connected
-	pub fn connect(&mut self,connection:usize,input:usize,output:usize){
-		(self.hidden,self.inputs,self.outputs)=(0,0,0);
-		self.connectivity.push((connection,input,output))
+	#[track_caller]
+	/// connects two graph between vertices with the connection specified by index. The connections run in the order they are connected.
+	pub fn connect<I:Into<GraphIO>,J:Into<GraphIO>>(&mut self,connection:usize,input:I,output:J){
+		let connections=self.connections.len();
+		let connectivity=&mut self.connectivity;
+
+		assert!(connection<connections,"connection index {connection} must exist. connections length was {connections}");
+		let (input,output)=(input.into().as_input_isize(),output.into().as_output_isize());
+		connectivity.push((connection,input,output));
 	}
 	/// adds a connection that doesn't anything yet
-	pub fn push(&mut self,connection:C)->usize{
+	pub fn push<A:Into<C>>(&mut self,connection:A)->usize{
 		let cx=self.connections.len();
-		self.connections.push(connection);
+		self.connections.push(connection.into());
 		cx
+	}
+}
+impl<E:Merge> Merge for Option<E>{
+	fn merge(&mut self,other:Self){
+		match (self,other){(Some(this),Some(other))=>this.merge(other),(this,Some(other))=>*this=Some(other),_=>()}
 	}
 }
 impl<E> Merge for Vec<E>{
 	fn merge(&mut self,other:Self){self.extend(other)}
 }
+#[macro_export]
+/// macro to get indices from formatted labels
+macro_rules! labeled{
+	($labels:ident,$($arg:tt)*)=>($labels.produce_index(format!($($arg)*)));
+}
+#[derive(Clone,Copy,Debug,Eq,PartialEq)]
+/// enum for whether a node index is a hidden input or output node
+pub enum GraphIO{Hidden(usize),Input(usize),Output(usize)}
 #[derive(Clone,Debug,Eq,PartialEq)]
 /// graph like ai operation structure. The connections run in the order they are connected
-pub struct Graph<C>{connections:Vec<C>,connectivity:Vec<(usize,usize,usize)>,hidden:usize,inputs:usize,outputs:usize}
+pub struct Graph<C>{connections:Vec<C>,connectivity:Vec<(usize,isize,isize)>}
+#[derive(Clone,Copy,Debug,Default,Eq,PartialEq)]
+/// wrapper for graph input node indices
+pub struct GraphIn(usize);
+#[derive(Clone,Copy,Debug,Default,Eq,PartialEq)]
+/// wrapper for graph output node indices
+pub struct GraphOut(usize);
+#[derive(Clone,Debug,Default)]
+/// formatted string to id mapper for node naming convenience
+pub struct VertexLabels(HashMap<Cow<'static,str>,usize>);
 /// trait to allow merging multiple outputs into one graph node
 pub trait Merge{
 	/// merges the other into self, taking out of other if convenient
 	fn merge(&mut self,other:Self);
 }
+pub use labeled;
 use crate::ai::{AI,Decompose,Op};
-use std::mem::take;
+use std::{borrow::Cow,collections::HashMap,mem::take};
