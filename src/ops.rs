f78aeab8c31bd16fbd508c864f5099f22ef1816b
Aili Nierengarten <125620224+8000thCube@users.noreply.github.com>
2025-08-04 00:19:14 -0700

    Update ops.rs

--- src/ops.rs
+++ src/ops.rs
@@ -2,10 +2,24 @@ impl Abs for f32{
 	fn abs(self)->Self::Output{f32::abs(self)}
 	type Output=f32;
 }
+impl SquaredError for f32{
+	fn squared_error(self,rhs:f32)->Self::Output{
+		let d=self-rhs;
+		d*d
+	}
+	type Output=f32;
+}
 /// trait to represent the operation
 pub trait Abs{
 	/// computes the operation
 	fn abs(self)->Self::Output;
+	/// the output type
+	type Output;
+}
+/// trait to represent the operation
+pub trait SquaredError<R=Self>{
+	/// computes the operation
+	fn squared_error(self,rhs:R)->Self::Output;
 	/// the output type
 	type Output;
 }
