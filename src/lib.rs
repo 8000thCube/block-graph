e8c60399bd42600361c6c9f976a89121b2a6b5e5
Aili Nierengarten <125620224+8000thCube@users.noreply.github.com>
2025-08-17 01:54:37 -0700

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 #![recursion_limit = "256"]
 mod ai;
 mod graph;
-/// builtin ai functions
+/// builtin ai layers and value types
 pub mod builtin;
-/// provides burn specific functionality
+/// ai layers and value types relating to the burn library
 pub mod burn;
-/// provides additional op traits
+/// operator like traits for common tensor operations
 pub mod ops;
 pub use {
 	ai::{AI,Decompose,Inner,IntoSequence,Op,UnwrapInner},graph::{Graph,Label,Merge,Unvec}
